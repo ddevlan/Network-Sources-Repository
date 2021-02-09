87286cb7e7824aa622f00027f5295eef89f38b15
Dylan Rodriguez <me@ddylan.com>
2021-02-09 00:53:43 -0500

    Updated master branch of deployment repo to point to latest head of submodules

--- Hydrogen-API/src/main/java/com/ddylan/hydrogen/api/repository/ChatFilterRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ddylan.hydrogen.api.repository;
-
-import com.ddylan.hydrogen.api.model.ChatFilter;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatFilterRepository extends MongoRepository<ChatFilter, String> {}
--- Hydrogen-API/src/main/java/com/ddylan/hydrogen/api/repository/ServerGroupRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ddylan.hydrogen.api.repository;
-
-import com.ddylan.hydrogen.api.model.ServerGroup;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServerGroupRepository extends MongoRepository<ServerGroup, String> {}
